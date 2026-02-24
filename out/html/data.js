aa7018d12a49e9ac23b095bbd6b6fb01994f556a
zlr <144011510+KenzieL-a@users.noreply.github.com>
2026-02-24 07:26:06 +0700

    tooltip

---
+++ out/html/data.js
@@ -0,0 +1,15 @@
+const colourList = [{
+        word: "PNI",
+        style: "color: #990000; font-weight: bold;"
+    },
+    {
+        word: "PKI",
+        style: "color: #c00000; font-weight: bold;"
+    },
+];
+
+const tooltipList = [{
+    searchString: "PNI",
+    explanationText: "<img src=img/pni.png> Indonesian National Party"
+    },
+];
